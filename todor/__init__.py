c2dd8a099bb17aef9aaaa263a106adee7f3548ef
Jesús Donayi Vázquez Silva <jesus.vazquez@exeltis.com>
2025-02-24 16:50:40 -0600

    Avance 24022025

--- todor/__init__.py
+++ todor/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+#Crea la extencion
+db= SQLAlchemy()
 def create_app():
     app =  Flask(__name__)
     # Configuracion del proyecto
     app.config.from_mapping(
         DEBUG = True, 
-        SECRETE_KEY = 'dev'
+        SECRETE_KEY = 'dev',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
     )
+
+    db.init_app(app)
+    
     # Registrar Blueprint
     from . import todo
     app.register_blueprint(todo.bp)
@@ -17,4 +23,6 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('index.html')
+    with app.app_context():
+        db.create_all()
     return app
